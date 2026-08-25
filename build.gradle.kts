3b7609445fe5a94bef9e01d5f684d25a5aa9bf62
Martin Vysny <martin@vysny.me>
2026-08-25 20:31:10 +0300

    0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "com.github.mvysny.vaadintabscope"
-    version = "0.3-SNAPSHOT"
+    version = "0.3"
 
     repositories {
         mavenCentral()
